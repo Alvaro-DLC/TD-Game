b778ba7cdcbefab1219a45a4779dc27376995f0c
FranciscoCamach0 <129994954+FranciscoCamach0@users.noreply.github.com>
2023-05-15 19:15:42 -0700

    byt

--- buy.hpp
+++ buy.hpp
@@ -9,36 +9,41 @@ class Buy
     public:
         Buy()
         {
-            // sf::RectangleShape buyBtn(sf::Vector2f(32.f, 32.f));
-            // buyBtn.setFillColor(sf::Color(255,255,255));
-           //if
-            if(!buyText.loadFromFile("buyBtn.png"))
-            {
-                std::cout << "Error loading buyBtn.png\n";
+            
 
+           if(!(img2.loadFromFile("buyBtn.png")))
+            {
+                std::cout << "Error loading Btn.png\n";
             }
 
-            buyBtn.setTexture(buyText);
-           
-
+            // buyBtn.setTexture(&buyText);
+            // buyBtn.setPosition(50.f, 120.f);
+            
+            sf::RectangleShape buyBtn(sf::Vector2f(200.f, 100.f));
+            buyBtn.setPosition(50.f, 120.f);
+            buyBtn.setTexture(&img2);
+            
 
         }
-        bool pressedBuy(float x, float y){
-            // if(x > 390.f && x <= 422.f && y >= 110.f && y <= 155.f) return true; else return false;}
-             if(x > 200.f && x <= 960.f && y >= 22.f && y <= 400.f) return true; else return false;}
+        // bool pressedBuy(float x, float y){
+        //     // if(x > 390.f && x <= 422.f && y >= 110.f && y <= 155.f) return true; else return false;}
+        //      if(x > 200.f && x <= 960.f && y >= 22.f && y <= 400.f) return true; else return false;}
+         bool pressedBuy(float x, float y){
+            if(x > 50.f && x <= 250.f && y >= 120.f && y <= 220.f) return true; else return false;}
 
           void set(float x,float y)                                                                                  
         {
             buyBtn.setPosition(sf::Vector2f(y, x));
         }
     
-        // sf::RectangleShape& getBuyBtn() {return buyBtn;}
-        sf::Sprite& getBuyBtn(){return buyBtn;}
+        sf::RectangleShape& getBuyBtn() {return buyBtn;}
+        // sf::Sprite& getBuyBtn(){return buyBtn;}
+        
 
     private:
-        // sf::RectangleShape buyBtn;
-        sf::Sprite buyBtn;
-        sf::Texture buyText;
+        sf::RectangleShape buyBtn;
+        // sf::Sprite buyBtn;
+        sf::Texture img2;
 };
 
 
